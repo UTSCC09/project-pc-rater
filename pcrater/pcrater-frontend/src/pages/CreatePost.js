1768810c4fcba977202ca590ec30cdfdd3fa9145
Mohammad Sajjad <mohammad.sajjad@mail.utoronto.ca>
2022-03-20 02:42:16 -0400

    completed the backend and frontend connection for creating posts

--- pcrater/pcrater-frontend/src/pages/CreatePost.js
+++ pcrater/pcrater-frontend/src/pages/CreatePost.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import {gql, useMutation} from "@apollo/client";
 import './CreatePost.css';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const CREATE_POST = gql`
 mutation createPost ($name: String!, $role: String!, 
@@ -27,12 +29,12 @@ $visibility: String!, $type: String!) {
 
 export default function Post() {
     const [name, setName] = useState('');
-    const [role, setRole] = useState('');
+    const [role, setRole] = useState('Student');
     const [course, setCourse] = useState('');
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    const [visibility, setVisibility] = useState('');
-    const [type, setType] = useState('');
+    const [visibility, setVisibility] = useState('Public');
+    const [type, setType] = useState('Question');
 
     const [ createPost ] = useMutation(CREATE_POST);
 
@@ -41,13 +43,13 @@ export default function Post() {
         createPost({  variables: { name, role, course, title, content, visibility, type } });
 
         setName('');
-        setRole('');
+        setRole('Student');
         setCourse('');
         setTitle('');
         setContent('');
         setRole('');
-        setVisibility('');
-        setType('');
+        setVisibility('Public');
+        setType('Question');
       };
 
 
@@ -55,28 +57,63 @@ export default function Post() {
         <div className='div-post'>
             <form className="complex_post_form" id="add_post_form" onSubmit={submit}>
                 <div className="post_title">Create a Post</div>
-                Full Name:<input type="text" id="username" class="form_element" placeholder="Full Name" input value={name} 
+                Full Name:<input type="text" id="username" className="form_element" placeholder="Full Name" input value={name} 
                 onChange={({ target }) => setName(target.value)} required/>
 
-                Role:<input type="text" id="role" class="form_element" placeholder="TA/PROFESSOR/STUDENT" input value={role} 
-                onChange={({ target }) => setRole(target.value)}required/>
+                Role:<Dropdown style={{ textAlign: "left", marginBottom: "5px", marginLeft: "5px", width: "30%" }}>
+                    <Dropdown.Toggle id="dropdown-basic">
+                        {role}
+                    </Dropdown.Toggle>
 
-                Course:<input type="text" id="course" class="form_element" placeholder="Course Code" input value={course} 
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => setRole("Student")}>Student</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setRole("TA")}>TA</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setRole("Professor")}>Professor</Dropdown.Item>
+                    </Dropdown.Menu>
+                    
+                </Dropdown>  
+                {/* <input type="text" id="role" class="form_element" placeholder="TA/PROFESSOR/STUDENT" input value={role} 
+                onChange={({ target }) => setRole(target.value)}required/> */}
+
+                Course:<input type="text" id="course" className="form_element" placeholder="Course Code" input value={course} 
                 onChange={({ target }) => setCourse(target.value)}required/>
 
-                Title:<input type="text" id="title" class="form_element" placeholder="Post Title" input value={title} 
+                Title:<input type="text" id="title" className="form_element" placeholder="Post Title" input value={title} 
                 onChange={({ target }) => setTitle(target.value)} required/>
 
-                Content:<textarea rows="5" id="content" class="form_element" placeholder="Details" input value={content} 
+                Content:<textarea rows="5" id="content" className="form_element" placeholder="Details" input value={content} 
                 onChange={({ target }) => setContent(target.value)} required></textarea>
 
-                Visibility:<input type="text" id="visibility" class="form_element" placeholder="Private/Public" input value={visibility} 
-                onChange={({ target }) => setVisibility(target.value)}required/>
+                Visibility:<Dropdown style={{ textAlign: "left", marginBottom: "5px", marginLeft: "5px", width: "30%" }}>
+                    <Dropdown.Toggle id="dropdown-basic">
+                        {visibility}
+                    </Dropdown.Toggle>
 
-                Type:<input type="text" id="type" class="form_element" placeholder="Question/Note/Poll" input value={type} 
-                onChange={({ target }) => setType(target.value)} required/>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => setVisibility("Public")}>Public</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setVisibility("Private")}>Private</Dropdown.Item>
+                    </Dropdown.Menu>
+                    
+                </Dropdown>
+                {/* <input type="text" id="visibility" class="form_element" placeholder="Private/Public" input value={visibility} 
+                onChange={({ target }) => setVisibility(target.value)}required/> */}
+
+                Type:<Dropdown style={{ textAlign: "left", marginBottom: "5px", marginLeft: "5px", width: "30%" }}>
+                    <Dropdown.Toggle id="dropdown-basic">
+                        {type}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => setType("Question")}>Question</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setType("Note")}>Note</Dropdown.Item>
+                        <Dropdown.Item onClick={() => setType("Poll")}>Poll</Dropdown.Item>
+                    </Dropdown.Menu>
+                    
+                </Dropdown>  
+                {/* <input type="text" id="type" class="form_element" placeholder="Question/Note/Poll" input value={type} 
+                onChange={({ target }) => setType(target.value)} required/> */}
                 
-                <button type="submit" class="btn">Create Post</button>
+                <button type="submit" className="btn">Create Post</button>
             </form>
         </div>
     );
